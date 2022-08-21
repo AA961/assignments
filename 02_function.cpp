d53757f159ed1285ad01a3b314a7c676a8bee9fc
AA961 <ammar.ahmed961@gmail.com>
2022-08-21 20:01:18 +0500

    function Assignment CPP

--- 02_function.cpp
+++ 02_function.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void calculate(int firstNumber, int secondNumber, int oprator)
+void calculate(int firstNumber, int secondNumber, char oprator)
 {
     switch (oprator)
     {

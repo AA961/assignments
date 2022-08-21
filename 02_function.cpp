9af3e7f23473f9e5b7a54493290cb5cfc0a3c1f7
AA961 <ammar.ahmed961@gmail.com>
2022-08-21 19:57:58 +0500

    function Assignment CPP

---
+++ 02_function.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void calculate(int firstNumber, int secondNumber, int oprator)
+{
+    switch (oprator)
+    {
+    case '+':
+        cout << firstNumber << " + " << secondNumber << " = " << firstNumber + secondNumber << endl;
+        break;
+    case '-':
+        cout << firstNumber << " - " << secondNumber << " = " << firstNumber - secondNumber << endl;
+        break;
+    case '/':
+        cout << firstNumber << " / " << secondNumber << " = " << firstNumber / secondNumber;
+        break;
+    case '*':
+        cout << firstNumber << " * " << secondNumber << " = " << firstNumber * secondNumber;
+        break;
+
+    default:
+        cout << "Invalid Oprator" << endl;
+    }
+}
+
+int main()
+{
+    int firstNumber, secondNumber;
+    char oprator;
+    cout << "Enter First Number, Oprator, Second Number:";
+    cin >> firstNumber >> oprator >> secondNumber;
+    calculate(firstNumber, secondNumber, oprator);
+}
